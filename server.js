6e55996a8074d0862e964a441b6efb428da93fa7
David <learnitmyway@users.noreply.github.com>
2020-08-09 17:42:31 +0200

    return video data in res body

--- server.js
+++ server.js
@@ -3,20 +3,23 @@ const app = express()
 const port = 4005
 
 app.get('/api/videos', (req, res) => {
-  return [
-    {
-      id: '1',
-      src: 'assets/rock-balance.mp4',
-    },
-    {
-      id: '2',
-      src: 'assets/mare.mp4',
-    },
-    {
-      id: '3',
-      src: 'assets/waterfall.mp4',
-    },
-  ]
+  const resBody = {
+    videos: [
+      {
+        id: '1',
+        src: 'assets/rock-balance.mp4',
+      },
+      {
+        id: '2',
+        src: 'assets/mare.mp4',
+      },
+      {
+        id: '3',
+        src: 'assets/waterfall.mp4',
+      },
+    ],
+  }
+  res.send(resBody)
 })
 
 app.listen(port, () => {
