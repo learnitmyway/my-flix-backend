3a5f9a571267b7e5b045223800401ffebb7e4c2d
David <learnitmyway@users.noreply.github.com>
2020-08-09 17:38:25 +0200

    api/videos endpoint with stubbed data

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/videos', (req, res) => {
+  return [
+    {
+      id: '1',
+      src: 'assets/rock-balance.mp4',
+    },
+    {
+      id: '2',
+      src: 'assets/mare.mp4',
+    },
+    {
+      id: '3',
+      src: 'assets/waterfall.mp4',
+    },
+  ]
 })
 
 app.listen(port, () => {
