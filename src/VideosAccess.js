177380ec2dfccbdea9f3ab36e3a180593047060d
David <learnitmyway@users.noreply.github.com>
2020-08-25 12:06:23 +0200

    add logging in videos access

--- src/VideosAccess.js
+++ src/VideosAccess.js
@@ -7,13 +7,28 @@ class VideosAccess {
   }
 
   async getAllVideos() {
-    // eslint-disable-next-line no-console
     console.log('getAllVideos')
 
     const result = await this.docClient
-      .scan({
-        TableName: this.tableName,
-      })
+      .scan(
+        {
+          TableName: this.tableName,
+        },
+        (err, data) => {
+          if (err) {
+            console.error(
+              'Unable to query. Error:',
+              JSON.stringify(err, null, 2)
+            )
+            throw err
+          } else {
+            console.log('Query succeeded.')
+            data.Items.forEach(({ videoId }) => {
+              console.log(' -', videoId)
+            })
+          }
+        }
+      )
       .promise()
 
     return result.Items
