3ce943032ac58dbab8fa1069fce06c8f2698387c
David <learnitmyway@users.noreply.github.com>
2020-08-25 11:57:39 +0200

    update main.tf to match

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,10 +5,10 @@ provider "aws" {
 resource "aws_dynamodb_table" "videos-table" {
   name           = "Videos"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key       = "VideoId"
+  hash_key       = "videoId"
 
   attribute {
-    name = "VideoId"
+    name = "videoId"
     type = "S"
   }
 }
